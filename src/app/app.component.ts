2f6cc27bfb34aff94879e2434117f531ca6e0c28
zia-esar <ziaesar@gmail.com>
2020-12-16 18:34:11 +0500

    logIn form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from './todo'
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { VirtualTimeScheduler } from 'rxjs';
+import { Todo } from './todo';
+import { Users } from './users';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +12,19 @@ import { Todo } from './todo'
 export class AppComponent {
   title = 'todo-app';
 
+  username;
+  userpwd;
+
   todoValue: string;
   list: Todo[] = [];
+
+  userList: Users[] = [
+    {username: 'ali', password: 'asdf'},
+    {username: 'zia', password: '1234'},
+    {username: 'hammad', password: 'zxcv'},
+  ];
+
+  constructor(private modalService: NgbModal){}
 
   ngOnInit(){
     this.list = [];
@@ -33,6 +47,44 @@ export class AppComponent {
     this.list = this.list.filter(item => item.id !== id);
   }
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-title'});
+  }
 
+  addUser(){
+    if(this.username !== "" && this.userpwd !== ""){
+      const newUser: Users = {
+        username: this.username,
+        password: this.userpwd
+      }
+      this.userList.push(newUser);
+    }else {
+      console.log("Fill all fields!!")
+    }
+    this.username = "";
+    this.userpwd = "";
 
+    for(let item of this.userList){
+      console.log(item);
+    }
+  }
+
+  login(){
+    if(this.username !== "" && this.userpwd !== ""){
+     var result = this.userList.filter(item => item.username == this.username && item.password == this.userpwd)
+     console.log(result)
+      if(result.length != 0){
+        console.log("welcome")
+      }else {
+        console.log("You are not registered!!")
+      }
+    }else {
+      console.log("All fields should be filled!!")
+    }
+
+    this.username = "";
+    this.userpwd = "";
+
+  }
 }
+
